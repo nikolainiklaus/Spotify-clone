84f4f291de8afe2ccdb802c469765b3d5ba99ba7
Jovellyn Quiapos <jovellynanne11@gmail.com>
2023-01-24 15:19:46 +0000

    cleaned up code plus added more fetch

--- artist-js.js
+++ artist-js.js
@@ -2,6 +2,8 @@
 // 384236
 // queen
 // 412
+//bruno
+//429675
 
 const getArtist = (id) => {
   fetch(`https://striveschool-api.herokuapp.com/api/deezer/artist/${id}`, {
@@ -27,6 +29,9 @@ const displayArtist = (data) => {
   artistImage.src = data.picture_medium;
   let artistProfileIMG = document.getElementById("artist-profile");
   artistProfileIMG.src = data.picture_small;
+
+  let fanNode = document.getElementById("fanbase");
+  fanNode.innerText = data.nb_fan + " monthly listeners";
 };
 
 const getSongsList = (id) => {
@@ -53,30 +58,24 @@ const displaySongPopular = (response) => {
   let songTableList = document.getElementById("artisit-popu-songs");
   let index = 1;
   songs.forEach((song) => {
-    songTableList.innerHTML += `  <th scope="row">
-    ${index}
-</th>
-<th class="image-area p-0">
-    <img class=""
-        src="${song.album.cover_medium}"
-        alt="">
-</th>
-<th>
-    ${song.title}
-</th>
-<th>
-${song.rank}
-</th>
-<th>
-    ${song.duration}
-</th>`;
+    songTableList.innerHTML += ` <td scope="row">${index}</td>
+    <td class="image-area p-0">
+    <img class="" src="${song.album.cover_medium}" >
+    </td>
+    <td>${song.title}</td>
+    <td>${song.rank}</td>
+    <td>${changeToMins(song.duration)}</td>`;
     index++;
   });
+};
+
+const changeToMins = (mins) => {
+  return Math.floor(mins / 60) + ":" + (mins % 60);
 };
 
 window.onload = () => {
   const URLparams = new URLSearchParams(window.location.search);
   const id = URLparams.get("id");
-  getArtist(384236);
-  getSongsList(384236);
+  getArtist(429675);
+  getSongsList(429675);
 };
